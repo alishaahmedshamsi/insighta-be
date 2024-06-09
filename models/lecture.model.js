b4afb4f2dc87af2e355f8f61229b864ed9e979d7
ali usaid <aliusaid55@gmail.com>
2024-06-09 03:34:13 -0700

    chore: Update lecture model to remove required fields

--- models/lecture.model.js
+++ models/lecture.model.js
@@ -3,16 +3,13 @@ import mongoose from 'mongoose'
 const lectureSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
     },
     description: {
         type: String,
-        required: true
     },
     teacher: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Teacher',
-        required: true
     },
     class:{
         type: mongoose.Schema.Types.ObjectId,
