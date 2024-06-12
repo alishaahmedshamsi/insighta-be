1e380f58aca4e034fb86120a2924d57ed37aeadf
Moazam Ali <mozalikorai@gmail.com>
2024-06-12 20:00:33 +0500

    chore: Add validation to check if subject already exists before creating it

--- controllers/subject.controller.js
+++ controllers/subject.controller.js
@@ -1,22 +1,33 @@
-import { createSubject, fetchSubject, findSubject } from '../models/subject.model.js';
-import { generateResponse, asyncHandler } from '../utils/helpers.js';
-import { STATUS_CODES } from '../utils/constants.js';
+import {
+	createSubject,
+	fetchSubject,
+	findSubject,
+} from "../models/subject.model.js";
+import { generateResponse, asyncHandler } from "../utils/helpers.js";
+import { STATUS_CODES } from "../utils/constants.js";
 
 export const createSubjects = asyncHandler(async (req, res, next) => {
+	const isSubjectExist = await findSubject({
+		school: req.user.id,
+		name: req.body.name,
+		class: req.body.class,
+	});
+	console.log(isSubjectExist);
+	if (isSubjectExist)
+		return next({
+			statusCode: STATUS_CODES.BAD_REQUEST,
+			message: "Subject already exist",
+		});
 
-    const isSubjectExist = await findSubject({school:req.user.id, name:req.body.name});
-    console.log(isSubjectExist);
-    if(isSubjectExist) return next({
-        statusCode:STATUS_CODES.BAD_REQUEST,
-        message:"Subject already exist"
-    })
+	req.body.school = req.user.id;
+	const data = await createSubject(req.body);
+	generateResponse(data, "Subjects created successfully", res);
+});
 
-        req.body.school = req.user.id;
-        const data = await createSubject(req.body);
-        generateResponse(data, 'Subjects created successfully', res);
-    })
-        
 export const getSubects = asyncHandler(async (req, res, next) => {
-    const data  = await fetchSubject({school:req.user.id,class:req.params.id});
-    generateResponse(data, 'Subjects fetched successfully', res);
-})
+	const data = await fetchSubject({
+		school: req.user.id,
+		class: req.params.id,
+	});
+	generateResponse(data, "Subjects fetched successfully", res);
+});
